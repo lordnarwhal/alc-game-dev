f8ed29bfd54ef10f3398d1a3cdcdfdf022d93471
joshuajames <joshua.j399@stu.nebo.edu>
2018-11-08 10:29:25 -0700

    added art to game

---
+++ 2d game/Assets/scripts/CameraFallow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFallow : MonoBehaviour {
+	public ChararterMove Player;
+	public bool isFallowing;
+
+	//camera position offset
+	public float xOffset;
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		Player = FindObjectOfType<ChararterMove>();
+		isFallowing = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFallowing){
+			transform.position = new Vector3(Player.transform.position.x + xOffset, Player.transform.position.y + yOffset, transform.position.z);
+
+		}
+		
+	}
+}
