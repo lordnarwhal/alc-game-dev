9cd1bb79c978c77f4c8cbc39ee848aaf88a1c774
joshuajames <joshua.j399@stu.nebo.edu>
2018-11-16 10:28:45 -0700

    got fixes working

--- 2d game/Assets/scripts/projectile.cs
+++ 2d game/Assets/scripts/projectile.cs
@@ -1,44 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class projectile : MonoBehaviour {
+
 	public float Speed;
+
 	public float TimeOut;
-	public GameObject pc;
+	public GameObject PC;
+
 	public GameObject EnemyDeath;
+
 	public GameObject ProjectileParticle;
+
 	public int PointsForKill;
 
 	// Use this for initialization
 	void Start () {
-		PC = GameObject.Find("pc");
-		EnemyDeath = Resources.Load("Prefabs/enemydeath") as GameObject;
-		ProjectileParticle = Resources.Load("Prefabs/RespawnES") as GameObject;
-		if(pc.transform.localScale.x < 0)
-				Speed = -Speed;
-			//Speed = Speed * Mathf.Sign(pc.transform.local.x);
-		//destroy projectile after x seconds
+		 PC = GameObject.Find("pc"); 
+
+		 EnemyDeath = Resources.Load("Prefabs/DeathES") as GameObject;
+
+		 ProjectileParticle = Resources.Load("Prefabs/RespawnES") as GameObject;
+
+		if(PC.transform.localScale.x < 0)
+			Speed = -Speed;
+
+			
+
+		// Destroys Projectile after X seconds
 		Destroy(gameObject,TimeOut);
+						
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
-
 	}
+
 	void OnTriggerEnter2D(Collider2D other){
+		//Destroys enemey on contact with projectile. Adds points. 
 		if(other.tag == "Enemy"){
-				Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
-				Destroy (other.gameObject);
-				ScoreManager.AddPoints (PointsForKill);
+			Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+			Destroy (other.gameObject);
+			ScoreManager.AddPoints (PointsForKill);
 		}
+		
+		
+		// Instantiate(ProjectileParticle, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
 
-			//Instantiate(ProjectileParticle, transform.position, transform.rotation);
-			Destroy (gameObject);
-
-		void OnCollisionEnter2D(Collision2D other);
+		void OnCollisionEnter2D(Collision2D other)
+	{
 		Instantiate(ProjectileParticle, transform.position, transform.rotation);
 		Destroy (gameObject);
+		
 	}
 }
